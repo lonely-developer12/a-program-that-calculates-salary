ab39c90ebe42802a4023407812ff282262080976
lonely-developer12 <emirbuzdic@gmail.com>
2024-07-04 18:25:59 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+hourlyrate = input('please enter the hourly rate. ')
+day = 8 * int(hourlyrate)
+month = day * 20
+year = month * 12
+print('Your hourly rate is ' + str(hourlyrate) + ' KM' + '.')
+print('You earn for the day ' + str(day) + ' KM' + '.')
+print('You earn for the month ' + str(month) + ' KM' + '.')
+print('You have a salary for the year ' + str(year) + ' KM' + '.')
+input('please press any key')
